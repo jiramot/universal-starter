ee2b2602a454eddf00da00745cf8446107a3c3ad
Jiramot Numnam <jiramot@gmail.com>
2016-06-20 21:12:36 +0700

    Refactor basic env
    remove duplicate code

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const commonConfig = require('./config/webpack.common.js');
+const clientConfig = require('./config/webpack.client.js');
+const serverConfig = require('./config/webpack.server.js');
+const helpers = require('./helpers');
+const config = require('config');
+
+var definePlugin = require('./webpack.dev');
+
 switch (process.env.NODE_ENV) {
   case 'prod':
   case 'production':
-    module.exports = require('./webpack.prod.js');
+    definePlugin = require('./webpack.prod.js');
     break;
   case 'test':
   case 'testing':
-    module.exports = require('./webpack.test.js');
+    definePlugin = require('./webpack.test.js');
     break;
   case 'dev':
   case 'development':
   default:
-    module.exports = require('./webpack.dev.js');
+    definePlugin = require('./webpack.dev.js');
 }
+
+// Default config
+var defaultConfig = {
+  context: __dirname,
+  resolve: {
+    root: [
+      helpers.root('/src')
+    ]
+  },
+  output: {
+    publicPath: path.resolve(__dirname),
+    filename: 'index.js'
+  },
+  plugins: [definePlugin]
+};
+
+var webpackMerge = require('webpack-merge');
+module.exports = [
+  // Client
+  webpackMerge({}, defaultConfig, commonConfig, clientConfig),
+
+  // Server
+  webpackMerge({}, defaultConfig, commonConfig, serverConfig)
+];
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,6 @@
 const webpack = require('webpack');
-const path = require('path');
-const commonConfig = require('./config/webpack.common.js');
-const clientConfig = require('./config/webpack.client.js');
-const serverConfig = require('./config/webpack.server.js');
-const helpers = require('./helpers');
 
-var definePlugin = new webpack.DefinePlugin({
-  __NAME__: JSON.stringify("prod"),
+
+module.exports = new webpack.DefinePlugin({
+  __NAME__: JSON.stringify("prod")
 });
-
-// Default config
-var defaultConfig = {
-  context: __dirname,
-  resolve: {
-    root: [
-      helpers.root('/src')
-    ]
-  },
-  output: {
-    publicPath: path.resolve(__dirname),
-    filename: 'index.js'
-  },
-  plugins: [definePlugin]
-};
-
-var webpackMerge = require('webpack-merge');
-module.exports = [
-  // Client
-  webpackMerge({}, defaultConfig, commonConfig, clientConfig),
-
-  // Server
-  webpackMerge({}, defaultConfig, commonConfig, serverConfig)
-];
